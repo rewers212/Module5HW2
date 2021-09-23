9737789c0be222b5e42c593b02cbe7a73b96481e
OMIS\Geiko_J <rewers212>
2021-09-23 14:41:24 +0300

    HW

---
+++ Module5HW2/Module5HW2/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module5HW2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public string Register([FromForm] User user)
+        {
+            return $"Email:{user.Email}\n" +
+                $"Password:{user.Password}";
+        }
+    }
+}
